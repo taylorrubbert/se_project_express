7a98cd18680121b5e1b0e81f7436896f563c2cfe
taylorrubbert <tayrubbert@hotmail.com>
2024-11-11 15:39:02 -0600

    adjustments to app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mainRouter = require("./routes/users.js");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -8,12 +8,20 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log("Connected to DB");
+    console.log("Connected do DB");
   })
   .catch(console.error);
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "67251b4383aac86858c8db57",
+  };
+  next();
+});
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening at port ${PORT}`);
 });
