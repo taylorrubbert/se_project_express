3eaf284e4bdd178069cf5d20b2740a0999b973b7
taylorrubbert <tayrubbert@hotmail.com>
2025-01-21 19:14:59 -0600

    backend progress

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
-const { JWT_SECRET = "dev-secret" } = process.env;
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
-module.exports = { JWT_SECRET };
+module.exports = {
+  JWT_SECRET,
+};
